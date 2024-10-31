e80f2fd3afb94213ce538ad5a3b9e81bc4e410b1
Divya Patel <Divya@Prathams-MacBook-Pro-2.local>
2024-10-30 20:06:46 -0400

    undid change

--- backend/test_main.py
+++ backend/test_main.py
@@ -9,7 +9,7 @@ from main import app  # Import your Flask app
 BASE_URL = "/"
 
 @pytest.fixture
-def test_client():
+def client():
     """Create a test client for the Flask app."""
     with app.test_client() as client:
         yield client
