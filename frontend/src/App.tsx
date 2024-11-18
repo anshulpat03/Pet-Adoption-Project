43b52eea547eed9839627bb682f3cc8b902dee5f
Divya Patel <Divya@Prathams-MacBook-Pro-2.local>
2024-11-18 17:14:39 -0500

    contact information popup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,17 +3,25 @@ import Navbar from "./components/Navbar.tsx"
 import './App.css'
 import Pets from "./pages/Pets.tsx";
 import Home from "./pages/Home.tsx";
+import Contact from "./components/Contact.tsx";
+import { useState } from "react";
 
 function App() {
+  const [isContactOpen, setIsContactOpen] = useState(false);
+
+  const handleOpenContact = () => setIsContactOpen(true);
+  const handleCloseContact = () => setIsContactOpen(false);
+
   return (
     <>
       <Router>
-        <Navbar />
+        <Navbar onContactClick={handleOpenContact}/>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/pets" element={<Pets />} />
         </Routes>
       </Router>
+      <Contact isOpen={isContactOpen} onClose={handleCloseContact} />
     </>
   )
 }
