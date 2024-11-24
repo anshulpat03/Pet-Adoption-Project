e8bd07bff8a4d5b1c420f0d7fdd818c6c84480f6
Jiahui Jennifer Weng Wu <jiahui_jennifer.weng_wu@uconn.edu>
2024-11-24 16:04:59 -0500

    Frontend code for adoption form

---
+++ frontend/src/pages/AdoptForm.tsx
@@ -0,0 +1,87 @@
+import React, { useState, useEffect } from "react";
+//import { useParams } from "react-router-dom";  // Import useParams to access the dynamic parameter
+
+const AdoptForm: React.FC = () => {
+  //const { user_id } = useParams<{ user_id: string }>();  // Get the user_id from the URL params
+
+  useEffect(() => {
+    console.log("Pet Adoption Form");  // Just logging the user ID for now
+  });
+
+  const [formData, setFormData] = useState({
+    name: "",
+    salary: "",
+    housing:"",
+    contact:"",
+    pet_name:"",
+  });
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const response = await fetch(`/form`, {  // Send request to Flask backend with user_id
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    });
+    const responseData = await response.json();
+    console.log(responseData);  // Handle the response from the backend
+  };
+
+  return (
+    <div>
+      <h1>Pet Adoption Form</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Name:</label>
+          <input
+            type="text"
+            value={formData.name}
+            onChange={(e) => setFormData({ ...formData, name: e.target.value })}
+            required
+          />
+        </div>
+        <div>
+          <label>Montly Salary:</label>
+          <input
+            type="interger"
+            value={formData.salary}
+            onChange={(e) => setFormData({ ...formData, salary: e.target.value })}
+            required
+          />
+        </div>
+        <div>
+          <label>Do you own or rent a place:</label>
+          <input
+            type="text"
+            value={formData.housing}
+            onChange={(e) => setFormData({ ...formData, housing: e.target.value })}
+            required
+          />
+        </div>
+        <div>
+          <label>How to contact you?</label>
+          <input
+            type="text"
+            value={formData.contact}
+            onChange={(e) => setFormData({ ...formData, contact: e.target.value })}
+            required
+          />
+        </div>
+        <div>
+        <label>Name of pet that you want to adopt:</label>
+        <input
+            type="text"
+            value={formData.pet_name}
+            onChange={(e) => setFormData({ ...formData, pet_name: e.target.value })}
+            required
+          />
+        </div>
+        <button type="submit">Submit Form</button>
+      </form>
+    </div>
+  );
+};
+
+export default AdoptForm;
